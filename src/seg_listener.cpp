01ac909d98d838b2ec1ba2c4c131e92479dd0219
Katerina Oikonomou <k.m.oikonomou@gmail.com>
2021-02-10 17:05:40 +0200

    Changes in order to save ros images as cv images

--- src/seg_listener.cpp
+++ src/seg_listener.cpp
@@ -15,12 +15,12 @@ void segCallback(const image_msgs::Image_Segments set){
 	int image_count = 0; 
 	for( int i=0; i<set.image_set.size(); i++ ){
 
-		sensor_msgs::Image msg =set.image_set[i];
-		cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");                       
-		std::stringstream sstream;                               
-		sstream << "my_image_" << message_count << "_" << image_count << ".png" ;                 
-		ROS_ASSERT( cv::imwrite( sstream.str(),  cv_ptr->image ) );      
-		image_count++;                                      
+		sensor_msgs::Image msg = set.image_set[i];
+		cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
+		std::stringstream sstream;
+		sstream << "my_image_" << message_count << "_" << image_count << ".png" ;
+		ROS_ASSERT_ENABLED(cv::imwrite( sstream.str(),  cv_ptr->image )) ;
+		image_count++;
 	}
 
 	std::cout << "Printing Header:\n" << set.header << std::endl;
