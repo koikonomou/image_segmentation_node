697b2719773ce2cc8e2fed1637018c90393334e1
Katerina Oikonomou <koikonomou@iit.demokritos.gr>
2021-05-25 22:33:40 +0300

    Publish image topic

--- src/image_seg.py
+++ src/image_seg.py
@@ -16,22 +16,19 @@ class Track:
     # pool_ = multiprocessing.Pool(multiprocessing.cpu_count())
 
     def __init__(self):
-      print("A")
-      # self.image_pub = rospy.Publisher("image_topic_2",Image)
 
       self.bridge = CvBridge()
       self.image_sub = rospy.Subscriber("seg_images", Image_Segments, self.track_callback)
+      self.image_pub = rospy.Publisher("Image_seg_color_track",Image)
       self.num_clusters = 1
 
 
     def find_dist_(self, prev_center, new_center):
-        print("4")
         """Find distance between two Lab color centers"""
         return 8 * np.sqrt((float(prev_center) - float(new_center)) ** 2 + (float(prev_center) - float(new_center)) ** 2) + 2 * abs(prev_center - new_center)
         # return 8 * np.sqrt((float(prev_center[1]) - float(new_center[1])) ** 2 + (float(prev_center[2]) - float(new_center[2])) ** 2) + 2 * abs(prev_center[0] - new_center[0])
 
     def find_all_dists_(self, centers):
-        print("5")
         """Find all distances between 2 lists of color centers"""
         ret = []
         for cen_i, center in enumerate(centers):
@@ -41,13 +38,11 @@ class Track:
 
     def update_all_dists_(self, all_dists, match):
         """Remove distances of centers that have already been matched"""
-        print("6")
         return [i for i in all_dists if (i[1][0] != match[0] and i[1][1] != match[1])]
 
     def update_cur_center_(self, matches, center):
         """Update current center"""
         matched = []
-        print("7")
         for match in matches:
             matched.append(match[0])
             self.centers[match[1]] = (center[match[0]] + self.centers[match[1]]) / 2
@@ -57,7 +52,6 @@ class Track:
 
     def find_dom_color_(self,image_gb):
         """Find dominant color of upper body part using kmeans clustering"""
-        print("8")
         img = cv2.cvtColor(image_gb, cv2.COLOR_BGR2LAB)
         img = img.reshape((img.shape[0] * img.shape[1], 3))
 
@@ -73,49 +67,41 @@ class Track:
         #Person mask is not eligible
         # return None
 
-    # def fix_masks_(self, masks, indices):
-    #     """Remove from every mask any common pixels with another mask"""
-    #     new_masks = []
-    #     for ind in indices:
-    #         mask = masks[ind]
-    #         for ind1 in indices:
-    #             if ind != ind1:
-    #                 mask = np.logical_and(mask, np.logical_and(masks[ind], np.logical_not(masks[ind1])))
-    #         new_masks.append(mask)
-    #     return new_masks
-
     def visualize_tracking_(self, img, indices, rect):
         """Show image with colored bounding boxes on people"""
         img1 = np.copy(img)
-        print("9")
         print("len indices", len(indices))
         for i in range(len(indices)):
             print("rectangle", rect[i])
             img1 = cv2.rectangle(img1, rect[i], self.colors_[self.ids[i] % len(self.colors_)], 2)
         cv2.imshow("image", img1)
         cv2.waitKey(1)
-        # cv2.destroyAllWindows()
+
+    def pub_image(self, img, indices, rect):
+        img1 = np.copy(img)
+        print("len indices", len(indices))
+        for i in range(len(indices)):
+            print("rectangle", rect[i])
+            image = cv2.rectangle(img1, rect[i], self.colors_[self.ids[i] % len(self.colors_)], 2)
+        return image
+
 
     def track_callback(self, msg):
-        print("1") 
         image_gb = msg.full_image
         image_gb = self.bridge.imgmsg_to_cv2(image_gb, "bgr8")
         image_gb = cv2.GaussianBlur(image_gb, (7, 7), 0)
 
-        # print(1)
         #Gaussian Blurring
         # image_gb = cv2.GaussianBlur(img, (7, 7), 0)
 
         #Indices of detected people in outputs
         instances = []
         indices = []
-        # indices = np.where(msg.has_image == 1)[0]
-        # print("indices",indices)
-        # masks = self.fix_masks_(instances.pred_masks.numpy(), indices)
+
         for i in range (len(msg.image_set)):
             x = msg.x[i]
             y = msg.y[i]
-            img = msg.image_set[i]
+            # img = msg.image_set[i]
             width = msg.width[i]
             height = msg.height[i]
             instance = (x,y,width,height)
@@ -126,21 +112,9 @@ class Track:
         for y in range(len(msg.has_image)):
             ind = np.where(msg.has_image[y] == 1)[0]
             indices.append(ind)
-            # if (msg.has_image[y]==1):
-            #   indices = np.array(msg.has_image[y])
-        #     else:
-        #       continue
-        print("2")
-        print("LEN Indices", len(indices))
-        # print("type instance",type(instances.astype(np.uint32)))
-        # print("instances", instances)
-        # print("indices", indices)
-        #Parallel computation of color centers
-        # centers = self.pool_.starmap(self.find_dom_color_, [(instances.pred_boxes.tensor[indices[ind_i]].numpy().astype(np.uint32), masks[ind_i], image_gb) for ind_i in range(len(indices))])
-        centers = self.find_dom_color_(image_gb)
-        # centers = pool.starmap(find_dom_color, [(instances.pred_boxes.tensor[indices[ind_i]].numpy().astype(np.uint32), masks[ind_i], image_gb, head_perc, upper_body_perc, 2) for ind_i in range(len(indices))])
 
-        # print(type(centers))
+        centers = self.find_dom_color_(image_gb)
+
 
         new_indices = []
         new_centers = []
@@ -157,11 +131,9 @@ class Track:
         # self.masks = [instances.pred_masks.numpy()[ind] for ind in indices]
 
         if not self.centers:
-            print("3A")
             self.centers = centers
             self.ids = [i for i in range(len(self.centers))]
         else:
-            print("3B")
             #Calculate distances between previous and current centers
             all_dists = self.find_all_dists_(centers)
             matches = []
@@ -185,7 +157,12 @@ class Track:
                     marker += 1
 
         self.visualize_tracking_(image_gb, indices, instances)
+        try:
+          self.image_message = self.bridge.cv2_to_imgmsg(self.pub_image(image_gb, indices, instances), "passthrough")
+        except CvBridgeError as e:
+          print(e)
 
+        self.image_pub.publish(self.image_message)
 
 
 def main(args):
